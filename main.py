e6092a7cfbac78d69faab0fc26c8d50c9ba65e60
yoav <shenkerman1@mail.tau.ac.il>
2025-09-29 15:04:01 +0300

    Add Redis caching for match predictions
    - Implement caching of API predictions in Redis for 24 hours
    - Generate unique cache keys based on input (homeTeam, awayTeam, date)
    - Return cached prediction if available, otherwise compute and store

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from pydantic import BaseModel
 from modelService import MatchPredictor
 from datetime import date
 from teamNameMapping import teamsEnum
+import redis
+import json
+from hashlib import sha256
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+def makeCacheKey(data: dict):
+	dataStr = json.dumps(data, sort_keys=True)
+	return sha256(dataStr.encode()).hexdigest()
+
 app = FastAPI()
 predictor = MatchPredictor()
 
@@ -21,7 +31,19 @@ def formatPrediction(target, homeTeam, awayTeam):
 
 @app.post("/predict")
 def predictMatch(data: MatchData):
+	key = makeCacheKey({
+		"homeTeam": data.homeTeam.value,
+		"awayTeam": data.homeTeam.value,
+		"date": str(data.date)
+	})
+	cached = r.get(key)
+	if cached:
+		return {"prediction": cached.decode("utf-8")}
+
 	x = predictor.predict(data.model_dump())
 	prediction = formatPrediction(x, data.homeTeam.value, data.awayTeam.value)
+
+	r.setex(key, 86400, prediction)
+
 	return {"prediction": prediction}
 
